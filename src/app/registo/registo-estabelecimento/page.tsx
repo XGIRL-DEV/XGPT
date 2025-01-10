2a7d0bc24f4f71fe9080b0d3bbb989a9ef593dc7
xgirl.fr <xgirl.fr@gmail.com>
2025-01-10 18:17:47 +0100

    try 10

--- src/app/registo/registo-estabelecimento/page.tsx
+++ src/app/registo/registo-estabelecimento/page.tsx
@@ -5,7 +5,7 @@ import {useDispatch, useSelector} from "react-redux";
 import supabase from "@/database/supabase";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
-import ListCategoriaBar from "@/components/register/list-categoria-bar";
+import ListCategoriaBar from "@/components/list-categoria-bar";
 import FiltroPrice from "@/components/filtros/filtro-tarifa";
 import FiltroDistrito from "@/components/filtros/filtro-distrito";
 import {updateClubsNome, updateClubsTelefone} from "@/actions/ClubsActions";
